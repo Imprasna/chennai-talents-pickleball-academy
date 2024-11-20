991efd5daec49a888305854f3f8fd8fdb99dafe6
ram <ramachandran052015@gmail.com>
2024-11-20 15:35:46 +0530

    app router change

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../views/pages/Home/Home"
 
+// App router 
 const router = createBrowserRouter([
     {path:'/', element:<Home/>}
 ])
