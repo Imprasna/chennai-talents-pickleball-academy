395f5aad2924e11e87b7a320870960093e0a365b
ram <ramachandran052015@gmail.com>
2024-11-20 16:34:36 +0530

    feat: nav bar added

--- src/views/pages/Home.tsx
+++ src/views/pages/Home.tsx
@@ -3,10 +3,12 @@ import Hero from "../features/home/Hero";
 import About from "../features/home/About";
 import Features from "../features/home/Features";
 import Footer from "../features/home/Footer";
+import Navbar from "../components/Navbar";
 
 const Home: FC = () => {
   return (
     <div className="font-sans">
+      <Navbar/>
       <Hero />
       <About />
       <Features />
