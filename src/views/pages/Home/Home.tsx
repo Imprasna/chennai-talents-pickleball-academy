4f86ad7b8dfe2740eb32b59cc6115f844158b975
ram <ramachandran.p@optisolbusiness.com>
2024-11-20 14:39:15 +0530

    setup: router added

---
+++ src/views/pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+
+const Home:FC = () => {
+
+    return (
+      <>
+        <h1 className='text-3xl font-bold underline'>Hey Welcome to Chennai talents pickle ball academy!</h1>
+      </>
+    )
+  }
+  
+  export default Home;
